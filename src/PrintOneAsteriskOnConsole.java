cd5f491243829736a616c765f33e66f9432f4a9e
Amandeep Singh <amandees@thoughtworks.com>
2015-08-20 15:31:15 +0530

    Print one asterisk to the console.

---
+++ src/PrintOneAsteriskOnConsole.java
@@ -0,0 +1,8 @@
+/**
+ * Created by amandeepsingh on 20/08/15.
+ */
+public class PrintOneAsteriskOnConsole {
+    public static void main(String[] args) {
+        System.out.println("*");
+    }
+}
